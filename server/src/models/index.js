2162cbcb858a6e745bcb5818911520f6e4f39b79
zuyzz <duyldreal@gmail.com>
2025-10-10 13:34:35 +0700

    sequelize config: remove timestamp

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -18,9 +18,9 @@ const sequelizeConfig = {
   }
 };
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(process.env[config.use_env_variable], sequelizeConfig);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(config.database, config.username, config.password, sequelizeConfig);
 }
 
 fs
