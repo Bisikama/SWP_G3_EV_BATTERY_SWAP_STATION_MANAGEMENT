20a8b81c1fff9f319979974351d659f96c7300b0
Bisikama <minhbao28032005@gmail.com>
2025-10-24 23:02:14 +0700

    Cập nhật lại api đổi pin

--- src/models/subscription.js
+++ src/models/subscription.js
@@ -87,6 +87,11 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.ENUM('active', 'inactive'),
         allowNull: false,
         defaultValue: 'inactive'
+      },
+      swap_count: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0
       }
     },
     {
